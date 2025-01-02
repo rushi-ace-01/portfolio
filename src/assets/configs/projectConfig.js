eade35c1a6d23540b97f45297c776c7e992a6e02
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2025-01-02 23:40:21 +0530

    projectConfig.js

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -1,94 +1,59 @@
-import {BiGitRepoForked} from "react-icons/bi";
-import {AiFillGithub, AiFillEye} from "react-icons/ai";
-import {ImBook} from "react-icons/im";
+import { BiGitRepoForked } from "react-icons/bi";
+import { AiFillGithub, AiFillEye } from "react-icons/ai";
+import { ImBook } from "react-icons/im";
 
-import sklearnGenetic from "../images/sklearn_genetic_opt.png"
-import kafkaMl from "../images/kafkaml.png"
-import portfolio from "../images/portfolio.png"
+/*import landslideModel from "../images/landslide_model.png";
+import penguinSpecies from "../images/penguin_species.png";*/
+import portfolioWeb from "../images/portfolio_web.png";
 
 import React from 'react'
 
 const projectConfig = [
     {
         id: "project-5",
-        title: "Sklearn Genetic Opt",
+        title: "Landslide Prediction",
         links: [
             {
                 name: "repo",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt",
+                url: "https://github.com/rushi-ace-01/landslide-prediction",
                 icon: <AiFillGithub/>,
             },
             {
                 name: "fork",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/fork",
+                url: "https://github.com/rushi-ace-01/landslide-prediction/fork",
                 icon: <BiGitRepoForked/>,
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/subscription",
-                icon: <AiFillEye/>,
-            },
-            {
-                name: "docs",
-                url: "https://sklearn-genetic-opt.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
             }
         ],
-        image: sklearnGenetic,
-        description: "scikit-learn models hyperparameters tuning and feature selection, using evolutionary algorithms.",
+        image:"https://upload.wikimedia.org/wikipedia/commons/b/b2/Landslide_in_Cusco%2C_Peru_-_2018.jpg",
+        description: "A CNN-based model for landslide prediction using historical data and live weather inputs.",
         target: "_blank"
     },
     {
         id: "project-3",
-        title: "Kafkaml Anomalies",
+        title: "Penguin Species App",
         links: [
             {
                 name: "repo",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection",
+                url: "https://github.com/rushi-ace-01/Penguin-species-App",
                 icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/subscription",
-                icon: <AiFillEye/>
             }
         ],
-        image: kafkaMl,
-        description: "Project for real-time anomaly detection using kafka and python.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIfOe9CR3Rs1tv0VOyakVUYEjQORfl-L-69A&s",
+        description: "Project for Penguin Species Predction",
         target: "_blank"
     },
     {
         id: "project-2",
-        title: "Pyworkforce",
+        title: "UI/UX Projects",
         links: [
             {
                 name: "repo",
-                url: "https://github.com/rodrigo-arenas/pyworkforce",
+                url: "https://github.com/rushi-ace-01/UI-UX-projects",
                 icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/subscription",
-                icon: <AiFillEye/>
-            },
-            {
-                name: "docs",
-                url: "https://pyworkforce.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
             }
         ],
-        image: "https://miro.medium.com/max/1400/1*UDnhOFK35IbAPx15wkMgVg.jpeg",
-        description: "Standard tools for workforce management, queuing, scheduling, rostering and optimization problems.",
+        image: "https://e7.pngegg.com/pngimages/292/269/png-clipart-logo-user-interface-design-user-experience-design-design-user-interface-design-label.png,
+        description: "Designs, prototypes and graphics illustrations",
         target: "_blank"
     },
     {
@@ -97,17 +62,17 @@ const projectConfig = [
         links: [
             {
                 name: "repo",
-                url: "https://github.com/rodrigo-arenas/portfolio",
+                url: "https://github.com/rushi-ace-01/portfolio",
                 icon: <AiFillGithub/>
             },
             {
                 name: "fork",
-                url: "https://github.com/rodrigo-arenas/portfolio/fork",
+                url: "https://github.com/rushi-ace-01/portfolio/fork",
                 icon: <BiGitRepoForked/>
             },
             {
                 name: "subscription",
-                url: "https://github.com/rodrigo-arenas/portfolio/subscription",
+                url: "https://github.com/rushi-ace-01/portfolio/subscription",
                 icon: <AiFillEye/>
             }
         ],
