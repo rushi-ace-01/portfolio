ede8e8a3fb66bf724c3bc6506f934bfc5360aa2b
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2025-01-03 12:27:58 +0530

    footerConfig.js

--- src/assets/configs/footerConfig.js
+++ src/assets/configs/footerConfig.js
@@ -1,6 +1,6 @@
 import {AiFillGithub} from "react-icons/ai";
 import {FaLinkedinIn} from "react-icons/fa";
-import {BsMedium} from "react-icons/bs";
+/*import {BsMedium} from "react-icons/bs";*/
 import React from "react";
 
 const footerConfig = {
